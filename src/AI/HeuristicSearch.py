c0cdc12285a6043a7305dec936dd370784d1ab33
DefinitelyNotJosh1 <krasnogo27@up.edu>
2025-09-16 22:11:02 -0700

    Get rid of has moved section

--- src/AI/HeuristicSearch.py
+++ src/AI/HeuristicSearch.py
@@ -77,22 +77,10 @@ def utility(gameState):
         if foodScore:
             utility += foodScore * 0.6
 
-        # defense stuff - 35% of total utility
+        # defense stuff - 40% of total utility
         defenseScore = defenseUtility(gameState, myInv, enemyInv, me)
         if defenseScore:
-            utility += defenseScore * 0.4
-
-        # has everyone moved - 5% of total utility (this just makes sure every ant moves in place, as it's good if we do)
-        myAnts = getAntList(gameState, me, (QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER))
-        hasMoved = 0
-        hasMovedScore = 0
-        for ant in myAnts:
-            if not ant.hasMoved:
-                hasMoved += 1
-        if len(myAnts) > 0 and hasMoved > 0:
-                hasMovedScore = hasMoved / len(myAnts)
-        
-        utility += hasMoved * 0.05
+            utility += defenseScore * 0.40
                 
 
 
@@ -407,6 +395,8 @@ if __name__ == "__main__":
     # if not isinstance(move, Move):
     #     print(f"ERROR: getMove() did not return a Move. Got: {move}")
 
+
+# BEST MOVE TEST
 print("Beginning bestMove test")
 nodes = []
 for i in range(10):
@@ -421,6 +411,7 @@ else:
     print(f"BestMove test failed. Value was {bestNode.evaluation}, expected 1.9")
 
 
+# UTILITY TEST
 print("Beginning utility test")
 gameState = GameState.getBlankState()
 util = utility(gameState)
@@ -430,3 +421,21 @@ else:
     print(f"Utility test passed. Value was {util}, expected 0.4")
 
 
+# # FOOD UTILITY TEST
+# print("Beginning food utility test")
+# gameState = GameState.getBasicState()
+# util = foodUtility(gameState, getCurrPlayerInventory(gameState), getEnemyInv(0, gameState), 0)
+# if not 0.0 <= util <= 1.0:
+#     print(f"ERROR: foodUtility() returned {util}")
+# else:
+#     print(f"Food utility test passed. Value was {util}, expected 0.0")
+
+
+# # DEFENSE UTILITY TEST
+# print("Beginning defense utility test")
+# gameState = GameState.getBasicState()
+# util = defenseUtility(gameState, getCurrPlayerInventory(gameState), getEnemyInv(0, gameState), 0)
+# if not 0.0 <= util <= 1.0:
+#     print(f"ERROR: defenseUtility() returned {util}")
+# else:
+#     print(f"Defense utility test passed. Value was {util}, expected 0.0")
